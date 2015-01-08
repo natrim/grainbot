7d8c7894167524baa01c761d379e2e0876d2e873
Natrim <maly@kinet.cz>
2015-01-08 13:25:46 +0100

    let bot use at leat 2cpus iv available

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/natrim/grainbot/modules"
 	"github.com/natrim/grainbot/modules/autojoin"
 	"github.com/natrim/grainbot/modules/fun"
@@ -10,6 +12,10 @@ import (
 var grainbot *Bot
 
 func main() {
+	if runtime.NumCPU() >= 2 { //if enough cpu's then use
+		runtime.GOMAXPROCS(2) //two of them
+	}
+
 	grainbot = NewBot()
 
 	//register modules
