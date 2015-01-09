780993a22d1b5bf2e9ccbae17a06b1edaa8db23b
Natrim <maly@kinet.cz>
2015-01-09 13:03:18 +0100

    add nick response + make join/part better

--- modules/system/system.go
+++ modules/system/system.go
@@ -10,22 +10,43 @@ import (
 // precompile the command regexp
 var quitreg = regexp.MustCompile("^quit$")
 var restartreg = regexp.MustCompile("^restart$")
-var joinpartreg = regexp.MustCompile("^(join|part) #([^ ]*)$")
+var joinpartreg = regexp.MustCompile("^(join|part)( #([^ ]*))?$")
+var nickreg = regexp.MustCompile("^nick ([^ ]*)$")
 
 // InitSystem register's dice commands on module load
 func InitSystem(mod *modules.Module) {
 	owner := &modules.OwnerPermission{}
 	mod.AddResponse(quitreg, func(r *modules.Response) {
+		r.Respond("okey, " + r.Nick + "! Goodbye everypony!")
 		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 	}, owner)
 	mod.AddResponse(restartreg, func(r *modules.Response) {
+		r.Respond("okey, " + r.Nick + "! Reebooot!")
 		syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
 	}, owner)
 	mod.AddResponse(joinpartreg, func(r *modules.Response) {
 		if r.Matches[1] == "join" {
-			r.Server.Join("#" + r.Matches[2])
+			if len(r.Matches)-1 >= 3 && r.Matches[3] != "" {
+				r.Server.Join("#" + r.Matches[3])
+				r.Respond("okey, " + r.Nick + "! let'z join " + "#" + r.Matches[3])
+			} else {
+				r.Mention("tell me where to join!")
+			}
 		} else if r.Matches[1] == "part" {
-			r.Server.Part("#" + r.Matches[2])
+			if len(r.Matches)-1 >= 3 && r.Matches[3] != "" {
+				r.Respond("okey, " + r.Nick + "! let'z leave " + "#" + r.Matches[3])
+				r.Server.Part("#" + r.Matches[3])
+			} else if r.Channel != "" {
+				r.Respond("okey, " + r.Nick + " leaving from here!")
+				r.Server.Part(r.Channel)
+			} else {
+				r.Mention("tell me from what to leave!")
+			}
 		}
+
+	}, owner)
+	mod.AddResponse(nickreg, func(r *modules.Response) {
+		r.Server.Nick(r.Matches[1])
+		r.Respond("okey, " + r.Nick + "! Let'z talk as another pony!")
 	}, owner)
 }
