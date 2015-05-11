1a052205a09f273079d066a8cd2ddaaf93d7b250
Natrim <maly@kinet.cz>
2015-05-11 08:31:50 +0200

    set logger

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
+	log "github.com/Sirupsen/logrus"
 	"github.com/natrim/grainbot/bot"
 	"runtime"
 )
+
+var debug = flag.Bool("debug", false, "Print debug messages?")
+
+func init() {
+	flag.Parse()
+
+	log.SetFormatter(&PrettyLogFormatter{})
+	if *debug == true {
+		log.SetLevel(log.DebugLevel)
+	}
+}
 
 func main() {
 	if runtime.NumCPU() >= 2 { //if enough cpu then use
